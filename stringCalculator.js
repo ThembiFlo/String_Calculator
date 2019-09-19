00e6322fcc8372038ea56c6001503399f2acc4c1
ThembiShabalala <thembi.shabalala@umuzi.org>
2019-09-19 16:30:18 +0200

    string calculator

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,21 +1,18 @@
-function calculator(num){
+function calculator(num) {
+	var del = num.replace(/^(\/.*\n)/, ' ');
+	var number = del.split(/[//|;|\n,]/);
 
-var del = num.replace(/^(\/.*\n)/, ' ');
-var number = del.split(/[//|;|\n,]/);
+	let answer = 0;
+	if (num === ' ') {
+		return 0;
+	}
+	for (let i = 0; i < number.length; i++) {
+		answer += parseInt(number[i]);
+	}
+	if (answer < 0) {
+		throw new Error('negatives not allowed');
+	}
 
-    let answer = 0;
-    if(num === " "){
-        return 0;
-    }
-    for(let i = 0; i < number.length; i++){
-        answer += parseInt(number[i]);
-    }
-if(answer < 0){
-    throw new Error('negatives not allowed');
-}
-
-    
-
-return answer;
+	return answer;
 }
 
--- stringCalculatorSpec.js
+++ stringCalculatorSpec.js
@@ -1,29 +1,37 @@
-describe("stringCalculator", function(){
-    
+describe('stringCalculator', function() {
+	it('should return zero when the string is empty', function() {
+		let answer = calculator(' ');
 
-    it("should return zero when the string is empty", function(){
-      let answer = calculator(" ")
-
-      expect(answer).toBe(0);
-    });
-    it("should return sum of two numbers separated by commas", function(){
-      let answer = calculator("1,2")
-      expect(answer).toBe(3);
-    });
-    it("should handle more than tw0 numbers", function(){
-      let answer = calculator("1,2,3,4")
-      expect(answer).toBe(10);
-    });
-    it("should handle new lines between numbers", function(){
-      let answer = calculator("1\n3")
-      expect(answer).toBe(4);
-    });
-    it("should remove the default delimiter and add numbers", function(){
-      let answer = calculator("//;\n2;4")
-      expect(answer).toBe(6);
-    });
-    it("should return 'negatives not allowed' when numbers are less than zero", function(){
-      let answer = function(){throw 'negatives not allowed'} 
-      expect(answer).toThrow('negatives not allowed');
-    });
-    })
+		expect(answer).toBe(0);
+	});
+	it('should return sum of two numbers separated by commas', function() {
+		let answer = calculator('1,2');
+		expect(answer).toBe(3);
+	});
+	it('should handle more than tw0 numbers', function() {
+		let answer = calculator('1,2,3,4');
+		expect(answer).toBe(10);
+	});
+	it('should handle new lines between numbers', function() {
+		let answer = calculator('1\n3');
+		expect(answer).toBe(4);
+	});
+	it('should remove the default delimiter and add numbers', function() {
+		let answer = calculator('//;\n2;4');
+		expect(answer).toBe(6);
+	});
+	it("should return 'negatives not allowed' when numbers are less than zero", function() {
+		let answer = function() {
+			throw 'negatives not allowed';
+		};
+		expect(answer).toThrow('negatives not allowed');
+	});
+	it('should ignore numbers bigger than 1000', function(){
+		let answer = calculator('2+1001');
+		expect(answer).toBe(2);
+	});
+	it('should allow multiple delimiters', function(){
+		let answer=calculator('//[*][%]\n1*2%3');
+		expect(answer).toBe(1);
+	});
+});
